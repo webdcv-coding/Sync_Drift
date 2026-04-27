3a0691488a611c84d82da20d157bc90bf722a255
webdcv-coding <webrapid.apps@gmail.com>
2026-04-27 20:49:26 +0100

    Changing files hoping for it to work (helplessly)
    just work

--- Sync-Drift.service.worker.js
+++ Sync-Drift.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777306965|831161254';
+const CACHE_VERSION = '1777318084|11949580154';
 /** @type {string} */
 const CACHE_PREFIX = 'Sync-Drift-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
