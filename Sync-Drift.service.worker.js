f13639784d46bef8596bc068607340949a5176b1
webdcv-coding <webrapid.apps@gmail.com>
2026-05-11 18:58:09 +0100

    Changed some of the game code to something else

--- Sync-Drift.service.worker.js
+++ Sync-Drift.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777398429|92294052995';
+const CACHE_VERSION = '1778522088|2277475635';
 /** @type {string} */
 const CACHE_PREFIX = 'Sync-Drift-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
